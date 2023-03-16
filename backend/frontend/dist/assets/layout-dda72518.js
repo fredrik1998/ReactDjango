a8a0f6cbb9e3e472752cae3fd83369acbf9d1a53
Fredrik Andrén <fredrik.andren98@gmail.com>
2023-03-16 14:50:01 +0100

    Made some changes to the filter feature

--- backend/frontend/dist/assets/layout-dda72518.js
+++
@@ -1,1 +0,0 @@
-import{j as t,m as o}from"./index-6b5cb7d4.js";const n=({children:i})=>t(o.div,{initial:{x:100,opacity:0},animate:{x:0,opacity:1},exit:{x:100,opacity:0},transition:{type:"spring",bounce:.5,velocity:1},children:i});export{n as L};
